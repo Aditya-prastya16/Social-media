d0098181c5c5f1975469e1732593c0d99017d76a
Aditya-prastya16 <adtyap87@gmail.com>
2024-07-07 22:34:57 +0800

    Social Media App

---
+++ src/app/components/MobileMenu.tsx
@@ -0,0 +1,28 @@
+'use client'
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+const MobileMenu = () => {
+    const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div className=''>
+        <div className='flex flex-col gap-[4.5px] cursor-pointer' onClick={() =>setIsOpen(prev => !prev)}>
+            <div className='w-6 h-1 bg-blue-500 rounded-sm'></div>
+            <div className='w-6 h-1 bg-blue-500 rounded-sm'></div>
+            <div className='w-6 h-1 bg-blue-500 rounded-sm'></div>
+        </div>
+        {isOpen && (
+            <div className=''> 
+                <Link href="">Home</Link>
+                <Link href="">Friend</Link>
+                <Link href="">Groups</Link>
+                <Link href="">Stories</Link>
+                <Link href="">Login</Link>
+
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default MobileMenu
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+import MobileMenu from './MobileMenu'
+
+const Navbar = () => {
+  return (
+    <div className='h-24 flex items-center justify-between'>
+        {/* left */}
+        <div className=''>
+            <Link href="/" className='font-bold text-xl text-blue-600'>
+                Dinagram
+            </Link>
+        </div>
+        {/* center */}
+        <div className='hidden'></div>
+        {/* right */}
+        <div className=''>
+            <MobileMenu />
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
